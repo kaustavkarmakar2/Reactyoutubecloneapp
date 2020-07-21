51283168cef07f06227d8d39498a92838bc3055d
kaustavkarmakar2 <kaustavkarmakar2@gmail.com>
2020-07-22 00:01:57 +0530

    Updates

---
+++ precache-manifest.bb307f676a045649c620b7857cf7ed90.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "431f5ac95d52245454258521c0315582",
+    "url": "/Reactyoutubecloneapp/index.html"
+  },
+  {
+    "revision": "dbfdd6c2c176e02df4fb",
+    "url": "/Reactyoutubecloneapp/static/js/2.914ff7cd.chunk.js"
+  },
+  {
+    "revision": "9acdbc3c792c62bd37fe",
+    "url": "/Reactyoutubecloneapp/static/js/main.106d0039.chunk.js"
+  },
+  {
+    "revision": "a89f680c46d25104dbdb",
+    "url": "/Reactyoutubecloneapp/static/js/runtime~main.80d6ab0b.js"
+  }
+]);
